8744c2c10323b2444841504924fadce3f9a951e6
blacklisss <rmjv@mail.ru>
2021-12-19 18:44:05 +0300

    Lesson 5 (Config Added)

--- Lesson_5/go.mod
+++ Lesson_5/go.mod
@@ -4,14 +4,14 @@ go 1.17
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/joho/godotenv v1.4.0
+	github.com/namsral/flag v1.7.4-pre
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.2.2
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 )
--- Lesson_5/main.go
+++ Lesson_5/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"l1g2/configuration"
 	cr "l1g2/services/crawler"
 	"l1g2/services/processor"
 	"l1g2/services/requester"
@@ -13,18 +14,21 @@ import (
 	"time"
 )
 
-const startUrl = "https://www.w3.org/Consortium/"
-
 func main() {
 	pid := os.Getpid()
 	log.Infof("My PID is: %d\n", pid)
 
+	config, err := configuration.Load("configuration/config.yaml")
+	if err != nil {
+		log.Fatalf("can't load configuration file")
+	}
+
 	var r cr.Requester
-	r = requester.NewRequester(time.Minute, startUrl)
+	r = requester.NewRequester(time.Minute, config.StartUrl)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	crawler := cr.NewCrawler(2, r)
-	crawler.Scan(ctx, startUrl, 0)
+	crawler.Scan(ctx, config.StartUrl, 0)
 
 	chSig := make(chan os.Signal)
 	signal.Notify(chSig, syscall.SIGTERM, syscall.SIGINT, syscall.SIGUSR1)
